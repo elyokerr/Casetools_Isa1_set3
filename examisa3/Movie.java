a060248cff0caf57017b5da416d5fa391803f78a
Samuel Demelo <samueldemelo0832@gmail.com>
2024-02-08 09:30:28 +0530

    push

---
+++ examisa3/Movie.java
@@ -0,0 +1,15 @@
+package examisa3;
+
+public class Movie {
+
+	private String title;
+	private String director;
+	public Movie(String title,String director) {
+		this.title=title;
+		this.director=director;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+}
